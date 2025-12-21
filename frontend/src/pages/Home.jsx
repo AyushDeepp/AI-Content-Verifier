dbf46c551b52380a4f21cb82ffcdd35b9b035925
AyushDeepp <ayushdeep8860@gmail.com>
2025-12-21 23:59:52 +0530

    fix 2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,8 +3,6 @@ import { Link, useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
 import {
     FaFileAlt,
-    FaImage,
-    FaVideo,
     FaShieldAlt,
     FaBolt,
     FaLock,
